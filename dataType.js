5c8f419c0d0fda5ec46b88fcb2c596f256d2a72c
robin0151 <ahmedrobin0151@gmail.com>
2023-05-04 23:30:56 +0600

    first commit

---
+++ dataType.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 'robin';
+let c = true;
+let d = 10.5;
+let e = null;
+let f ;
+let g = false;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
---
+++ variable.js
@@ -0,0 +1,14 @@
+console.log('10 + 15 = ' + (10 + 15));
+console.log('10 + 15 = ' + (10 - 15));
+console.log('10 + 15 = ' + (10 * 15));
+console.log('10 + 15 = ' + (10 / 15));
+
+
+let a = 35; 
+let b = 40;
+let c = a + b;
+
+console.log(a + ' + ' + b + ' = ' + c);
+console.log(a + ' - ' + b + ' = ' + (a - b));
+console.log(a + ' * ' + b + ' = ' + (a * b));
+console.log(a + ' / ' + b + ' = ' + (a / b));
