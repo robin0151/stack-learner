3ed628b515a58f5af22745a9861d79755b08a381
robin0151 <ahammedrobin1952@gmail.com>
2023-05-05 00:53:05 +0600

    added increament

---
+++ increament.js
@@ -0,0 +1,25 @@
+let postIncrement = 9;
+console.log(postIncrement++);
+console.log(postIncrement);
+
+let preIncrement = 10;
+console.log(++preIncrement);
+console.log(preIncrement);
+
+
+let n = 5;
+let m = (10) + ++n;
+
+console.log(m);
+console.log(n);
+
+let a = 5;
+
+console.log(a + ' ' + ++a + ' ' + a++);
+
+console.log(a);
+
+let d = 5;
+console.log(d + ' ' + --d + ' ' + d--);
+
+console.log(d);
